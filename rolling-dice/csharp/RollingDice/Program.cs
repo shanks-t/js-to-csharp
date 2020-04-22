6e86cdc95b5d15d06ce20542f27637c7f987d6b8
Andy Collins <askingalot@gmail.com>
2020-04-22 16:33:03 -0500

    rolling dice csharp code

---
+++ rolling-dice/csharp/RollingDice/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace RollingDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's roll some dice, baby!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Die die1 = Roll();
+                Die die2 = Roll();
+
+                string message = $"{die1} + {die2} == {die1.Value + die2.Value}";
+                if (die1.Value == die2.Value)
+                {
+                    message += " DOUBLES!";
+                }
+
+                Console.WriteLine(message);
+            }
+        }
+
+        static Die Roll()
+        {
+            int value = new Random().Next(1, 6);
+            Die die = new Die()
+            {
+                Value = value
+            };
+            return die;
+        }
+    }
+
+    public class Die
+    {
+        public int Value { get; set; }
+        public override string ToString()
+        {
+            string dieString = "Unknown";
+            switch (Value)
+            {
+                case 1:
+                    dieString = "\u2680";
+                    break;
+                case 2:
+                    dieString = "\u2681";
+                    break;
+                case 3:
+                    dieString = "\u2682";
+                    break;
+                case 4:
+                    dieString = "\u2683";
+                    break;
+                case 5:
+                    dieString = "\u2684";
+                    break;
+                case 6:
+                    dieString = "\u2685";
+                    break;
+            }
+
+            return dieString;
+        }
+    }
+}
