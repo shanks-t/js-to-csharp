99e206ae195ac9b0d481b0df8e045fe350d0a1c8
Andy Collins <askingalot@gmail.com>
2021-03-01 08:50:29 -0600

    Update main.js

--- dinner-party/js/dinnerParty/main.js
+++ dinner-party/js/dinnerParty/main.js
@@ -57,7 +57,7 @@ function getGuests() {
       bio: "(1912 - 1954) - British computing pioneer, Turing machine, algorithms, cryptology, computer architecture, saved the world"
     },
     {
-      name: "Admiral Grace Hopper (1906–1992) – developed early compilers: FLOW-Matic, COBOL; worked on UNIVAC; gave speeches on computer history, where she gave out nano-seconds",
+      name: "Admiral Grace Hopper",
       occupation: "computer scientist",
       bio: "(1906–1992) - developed early compilers: FLOW-Matic, COBOL; worked on UNIVAC; gave speeches on computer history, where she gave out nano-seconds"
     },
